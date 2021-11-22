4d76e00beef1399fc3a37d4a3629a10a11b75378
lidahan <lidh@dingdao.com>
2021-11-22 10:13:56 +0800

    init

---
+++ GinHelloWorldCode/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+	engine.GET("/hello", func(context *gin.Context) {
+		fmt.Println("请求路径： ", context.FullPath())
+		context.Writer.Write([]byte("Hello, gin\n"))
+	})
+	if err := engine.Run(":8090"); err != nil {
+		log.Fatal(err.Error())
+	}
+}
